a094e003671f4bf7d1d2a8c6e2a9f40ce1be423a
luismescobarf <luismescobarf@gmail.com>
2021-01-28 20:50:53 -0500

    Esqueleto inicial de un archivo fuente

---
+++ codigoFuente.go
@@ -0,0 +1,7 @@
+//Esqueleto de versiones
+
+//El desarrollador 1 trabaja en esta funcionalidad
+
+//El desarrollador 2 trabaja en esta funcionalidad
+
+//El desarrollador 3 trabaja en esta funcionalidad
