f6628c9e1c78849ec58eeb41f90f54400a2237dd
Mcamila-gil <manchitas417@hotmail.com>
2021-02-16 23:47:51 -0500

    programas

---
+++ ejercicio1Camila.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main(){
+var l int
+fmt.Println("Ingrese el tamaño del lado de un cuadrado ")
+		fmt.Scan(&l)
+		l*=4
+		fmt.Println("El perímetro del cuadrado es",l)
+}
---
+++ ejercicio2.go
@@ -0,0 +1,11 @@
+package	main
+import "fmt"
+func main(){
+	var num1, num2 int
+
+		fmt.Println("Ingrese numero")
+		fmt.Scan(&num1)
+		fmt.Scan(&num2)
+		num2=num1+num2
+
+}
