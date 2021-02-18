ffccd7c92fa3a6cc26e572a01030c785e30b8fad
MelanyT <melany-torresj@unilibre.edu.co>
2021-02-18 15:34:42 -0500

    Código actualizado

--- Taller1Melany/Suma-Melany.go
+++ Taller1Melany/Suma-Melany.go
@@ -1,8 +1,19 @@
 package  main 
 import "fmt"
 
-func main (){
-var 
-
-
+func main(){
+var n1, n2, n3, n4 int
+fmt.Print("Ingrese el primer numero: ")
+fmt.Scan(&n1)
+fmt.Print("Ingrese el segundo numero: ")
+fmt.Scan(&n2)
+fmt.Print("Ingrese el tercer numero: ")
+fmt.Scan(&n3)
+fmt.Print("Ingrese el cuarto numero: ")
+fmt.Scan(&n4)
+suma := n1+n2
+fmt.Println("La suma de los primeros nuemeros es: ", suma)
+multi := n3*n4
+fmt.Println("La multiplicacion de los ultimos numeros es: ", multi)
 }
+
