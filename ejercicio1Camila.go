cb8dec7ea2d81ac591ee319c0142b79d5a40dd31
Mcamila-gil <manchitas417@hotmail.com>
2021-03-16 11:54:40 -0500

    Eliminar archivos repetidos

--- ejercicio1Camila.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-func main(){
-var l int
-fmt.Println("Ingrese el tamaño del lado de un cuadrado ")
-		fmt.Scan(&l)
-		l*=4
-		fmt.Println("El perímetro del cuadrado es",l)
-}
