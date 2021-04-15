3c5b6a660ab52bfd859afcc26d630403d3c539e3
luismescobarf <luismescobarf@gmail.com>
2021-04-15 11:27:07 -0500

    Correcciones GCD Sincronico Taller

--- tallerConcurrenciaSingleMachine/correccionPunto1Sincronico.go
+++ tallerConcurrenciaSingleMachine/correccionPunto1Sincronico.go
@@ -1,28 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-type valores struct {
-	x int
-	y int
-}
+//Estructura de sincronización de goroutines
+var wg sync.WaitGroup
 
-func proceso1(tupla valores, p12 chan valores, p21 chan valores) {
+func proceso1(x int, y int, canalGeneral chan int) {
+
+	//Avisar terminación de proceso 1 al goroutine main que está en espera
+	defer wg.Done()
 
 	for {
 
-		if tupla.y != 0 {
+		//Salida de diagnóstico
+		//fmt.Println("Proceso 1 Trabajando!")
+
+		if y != 0 {
 
 			//Envío sincrónico
-			p12_x <- x
-			p12_y <- y
+			canalGeneral <- x
+			canalGeneral <- y
 
-			////Proceso 2
-			go proceso2(p12, p21)
+			fmt.Println("Envío realizado 1 !")
 
 			//Recepción sincrónica
-			y = <-p21_x
-			x = <-p21_y
+			y = <-canalGeneral
+			x = <-canalGeneral
+
+			fmt.Println("Recepción realizada 1 !")
 
 		} else {
 
@@ -33,54 +41,91 @@ func proceso1(tupla valores, p12 chan valores, p21 chan valores) {
 
 	}
 
+	fmt.Println("P1 termina!")
+
 }
 
-func proceso2(p12 chan valores, p21 chan valores) {
+func proceso2(canalGeneral chan int) {
+
+	//Avisar terminación de proceso 2 al goroutine main que está en espera
+	defer wg.Done()
 
 	var u, v, a int
 
-	tupla := <-p12
-	u = tupla.x
-	v = tupla.y
-
-	a = u
-
 	for {
 
-		a = a - v
-		if a < 0 {
+		//Recepción sincrónica
+		u = <-canalGeneral
+		v = <-canalGeneral
+
+		fmt.Println("Recibido en 2: u=", u, "v=", v)
+
+		a = u
+
+		for {
+
+			a = a - v
+			if a < 0 {
+				break
+			} else {
+				u = a
+			}
+
+		}
+
+		fmt.Println("P2 Enviando!! u=", u, "v=", v)
+
+		//Envío sincrónico
+		canalGeneral <- u
+		canalGeneral <- v
+
+		if u == 0 {
 			break
-		} else {
-			u = a
 		}
 
 	}
 
-	//Envío sincrónico
-	tupla.y = u
-	tupla.x = v
-	p21 <- tupla
+	fmt.Println("P2 termina!")
+
+}
+
+//Obtener el maximo comun divisor single threaded
+func gcd(x int, y int) int {
+
+	for {
+		if x > 0 && y == 0 {
+			return x
+		} else {
+			r := x % y
+			x = y
+			y = r
+		}
+	}
 
 }
 
 func main() {
 
 	var x, y int
-	x = 9
-	y = 6
+	x = 60
+	y = 45
 
-	tupla := make(valores)
-	tupla.x = x
-	tupla.y = y
+	//Llamado single threaded
+	//fmt.Println("El GCD de ", x, " y ", y, " es -> ", gcd(x, y))
 
 	//Diseño distribuído y sincrónico
-	p12 := make(chan valores)
-	p21 := make(chan valores)
+	canalGeneral := make(chan int)
+
+	//Sincronización del main con las dos goroutines
+	wg.Add(2)
 
 	//Lanzamiento del proceso 1
-	go proceso1(tupla, p12, p21)
+	go proceso1(x, y, canalGeneral)
 
-	var parar string
-	fmt.Scan(&parar)
+	//Lanzamiento del proceso 2
+	go proceso2(canalGeneral)
+
+	//Esperar a que los dos procesos terminen
+	wg.Wait()
 
 }
